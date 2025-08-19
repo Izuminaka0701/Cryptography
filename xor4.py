4bf949b38cc148ce449ea47b1b7409c7a741c74c
izumi <izuminaka071@gmail.com>
2025-08-20 00:55:18 +0700

    upload file

--- xor4.py
+++ xor4.py
@@ -1,6 +1,11 @@
-cryp = bytes.fromhex("0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104") # Mã hóa từ hex sang bytes
+# cryp = bytes.fromhex("0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104") # Mã hóa từ hex sang bytes
 
 
-key = b"myXORkey" # Tính khóa bằng XOR với ký tự 'c'
-print(''.join(chr(c ^ key[i % len(key)]) for i, c in enumerate(cryp))) # Giải mã
+# key = b"myXORkey" # Tính khóa bằng XOR với ký tự 'c'
+# print(''.join(chr(c ^ key[i % len(key)]) for i, c in enumerate(cryp))) # Giải mã
         
+        
+from pwn import xor
+flag = bytes.fromhex('0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104')
+print(xor(flag, 'crypto{'.encode())) # oh, it says 'myXORke+y...'
+print(xor(flag, 'myXORkey'.encode())) # try this? yay, it works! sometimes simpler is better
